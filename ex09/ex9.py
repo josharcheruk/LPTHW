7a43135905c70df7cf0faea389196988baf12c44
Josh Archer <josh@jamail.co.uk>
2016-02-08 16:25:26 +0000

    Corrects Exercise 9 spelling mistake

--- ex09/ex9.py
+++ ex09/ex9.py
@@ -5,7 +5,7 @@ print "Here are the days: ", days
 print "Here are the months: ", months
 
 print """
-Ther's something going on here.
+There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
