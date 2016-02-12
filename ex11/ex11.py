93d516611884254acc91b6b435b2b38f0e125960
Josh Archer <josh@jamail.co.uk>
2016-02-12 13:25:46 +0000

    Completes extra study drills for ex11 and ex13

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -5,4 +5,10 @@ height = raw_input()
 print "How much do you weigh?"
 weight = raw_input()
 
-print "So you are %r years old, %r tall and weigh %r." % (age,height,weight)
+print "So you are %r years old, %r tall and weigh %r." % (
+    age,height,weight)
+
+# A new form for the study drill
+
+name = raw_input("What is your name? >>> ")
+print 'Hey %s, let\'s be friends!' % name
---
+++ ex13/ex13-2.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+eat, food, echo, tree, time = argv
+
+print "The script is called:", eat
+print "Your first variable is called:", food
+print "Your second variable is called:", echo
+print "Your third variable is called:", tree
+print "Your third variable is called:", time
+
+raw_input(food)
+print "You like to eat: %s" % food
